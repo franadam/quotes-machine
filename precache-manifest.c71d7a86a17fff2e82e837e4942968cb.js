2815665d2edaa643311d3f5fe7ee1d7b376c1e13
franadam <francoisadam@hotmail.be>
2020-06-30 14:46:13 +0200

    Updates

---
+++ precache-manifest.c71d7a86a17fff2e82e837e4942968cb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "432a98b70f753b0d5ce91a9684a1baa3",
+    "url": "/quotes-machine/index.html"
+  },
+  {
+    "revision": "16ef9d05ba031493f48f",
+    "url": "/quotes-machine/static/css/main.e10ddb57.chunk.css"
+  },
+  {
+    "revision": "286abe0160ce707b5dae",
+    "url": "/quotes-machine/static/js/2.e56b6d40.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/quotes-machine/static/js/2.e56b6d40.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "16ef9d05ba031493f48f",
+    "url": "/quotes-machine/static/js/main.3a9df22e.chunk.js"
+  },
+  {
+    "revision": "628ad844eda44c11bbba",
+    "url": "/quotes-machine/static/js/runtime-main.c6a28ac5.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/quotes-machine/precache-manifest.c71d7a86a17fff2e82e837e4942968cb.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/quotes-machine/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
